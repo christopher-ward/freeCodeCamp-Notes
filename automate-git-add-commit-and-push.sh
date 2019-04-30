4229fd00afbb3824fe3cc9e01e0dea5ea428bac7
chrisward90 <chrsward90@gmail.com>
2019-04-29 23:41:31 -0700

    New logs and new changes to automation script

--- automate-git-add-commit-and-push.sh
+++ automate-git-add-commit-and-push.sh
@@ -1,8 +1,28 @@
+#!/bin/bash
+# Show status of git and prompt User for commit message to commit all the new changes to git and push to github
+# Also create conditionals to check for lack of message and also if User wishes to cancel commit after seeing status.
+
 git status
 
-git add .
+read -p 'commit -m ' varCommit
 
-git commit -m "Created new script to automate pushing new content to github"
+if [ -z "$varCommit" ]; then
+	echo "Please put in a commit message."
+	exit
+elif ["$1" == "n"]; then
+	echo "You canceled the script."
+	exit
+else
+	echo $varCommit
+fi
 
-git push
+#git add .
+
+#git commit -m ""$1""
+
+#git push
+
+echo "After the conditional."
+
+
 
