5645d79b1fa9a0fd1489d9b47918684eba6e3a38
chrisward90 <chrsward90@gmail.com>
2019-05-07 18:26:22 -0700

    Changing script

--- git-all-push.sh
+++ git-all-push.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 # Show status of git and prompt User for commit message to commit all the new changes to git and push to github
 # Also create conditionals to check for lack of message and also if User wishes to cancel commit after seeing status.
+
 echo ""
 
 git status
@@ -10,22 +11,36 @@ echo "Enter 'n' to cancel"
 echo ""
 read -p 'commit -m ' varCommit
 
-if [ -z "$varCommit" ]; then
+if [ -z "$varCommit" ]; 
+	then
 	echo ""
 	echo "Please put in a commit message."
 	exit
-elif [ "$varCommit" == "n" ] || [ "$varCommit" == "'n'" ]; then
+elif [ "$varCommit" == "n" ] || [ "$varCommit" == "'n'" ]; 
+	then
 	echo ""
 	echo "You canceled the script."
 	exit
+elif [ "$varCommit" == "spec" ]; 
+	then
+	read -p 'Specificy file to add commit and push: ' specFile
+	echo "Add '$specFile'? y/n"
+	read -p '' choice
+	if [ "$choice" == "y" ]; 
+		then
+		git add "$choice"
+		read -p 'Commit -m ' specCommit
+		git commit -m "$specCommit"
+	elif [ "$choice" == "n" ]; 
+		then
+		echo "Try again"
+		exit
+	fi
+	
 else
-	echo 'Thanks!'
-	echo ""
+	git add .
+	git commit -m "$varCommit"
 fi
-
-git add .
-
-git commit -m "$varCommit"
 
 git push
 
